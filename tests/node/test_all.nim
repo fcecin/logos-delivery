f7d9f07284ed540fafd2fb3063b14357c6ba9e14
Fabiana Cecin <fabiana@waku.org>
2026-09-24 11:54:54 -0300

    test(mix): end-to-end mix transport suite
    * add an end-to-end mix transport suite in `tests/node/test_wakunode_mix_e2e.nim`
    * register it in `tests/node/test_all.nim`

--- tests/node/test_all.nim
+++ tests/node/test_all.nim
@@ -5,6 +5,7 @@ import
   ./test_wakunode_legacy_lightpush,
   ./test_wakunode_lightpush,
   ./test_wakunode_mix,
+  ./test_wakunode_mix_e2e,
   ./test_wakunode_peer_exchange,
   ./test_wakunode_store,
   ./test_wakunode_peer_manager,
